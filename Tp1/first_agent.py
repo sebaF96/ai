c9bb8d0fb913c1608d39a2302c4f4b7cfafbab07
sebaf96 <sefernandez96@gmail.com>
2021-08-09 19:20:38 -0300

    Add comments to first_agent.py

--- Tp1/first_agent.py
+++ Tp1/first_agent.py
@@ -34,22 +34,27 @@ def start(floor):
     print("Starting floor:")
     print(floor)
     while True:
-        input()
+        sleep(2)
         movements += 1
         if is_dirty(floor[position]) and last_movement != "Clear":
+            # Position dirty and didn't clean in last movement
             clear(floor, position)
             last_movement = f"Clear"
         elif position != 0 and direction == "left":
+            # Moving left and can keep moving
             last_movement = "Move left"
             position -= 1
         elif position + 1 <= len(floor) - 1 and direction == "right":
+            # Moving right and can keep moving
             last_movement = "Move right"
             position += 1
         elif position + 1 == len(floor):
+            # Moving right and reach last index
             direction = "left"
             last_movement = "Move left"
             position -= 1
         elif position == 0 and direction == "left":
+            # Moving left and reach index 0
             direction = "right"
             last_movement = "Move right"
             position += 1
