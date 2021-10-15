193f8d47f70d108bf9dfef046d1eebffc4382457
sebaf96 <sefernandez96@gmail.com>
2021-10-15 16:57:34 -0300

    Fix Cleaner
    delete twitter url regexs

--- NLP/pipeline/nodes/Cleaner.py
+++ NLP/pipeline/nodes/Cleaner.py
@@ -9,10 +9,12 @@ class Cleaner(PipelineNode):
 
     def handle(self, df: pd.DataFrame) -> pd.DataFrame:
         URL_REGEX = r"(http:\/\/|https:\/\/|www\.)[a-zA-Z0-9]+(\.[a-zA-Z]+)+(/\w+)*(.\w+)*/? "
+        TWITTER_URL_REGEX = r'https:\/\/t\.co\/[a-zA-Z0-9\-\.]{10}'
         MENTIONS_REGEX = r"(?<=^|(?<=[^a-zA-Z0-9-_\.]))@([A-Za-z]+[A-Za-z0-9-_]+)"
         HASHTAG_REGEX = r"#"
         EMOJIS_REGEX = r"[^A-Za-z0-9 | \n]+"
         df["text"].replace(URL_REGEX, '', regex=True, inplace=True)
+        df["text"].replace(TWITTER_URL_REGEX, '', regex=True, inplace=True)
         df["text"].replace('https', '', regex=True, inplace=True)
         df["text"].replace(MENTIONS_REGEX, '', regex=True, inplace=True)
         df["text"].replace(HASHTAG_REGEX, '', regex=True, inplace=True)
