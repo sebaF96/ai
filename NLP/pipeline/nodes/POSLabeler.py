09038fb48d94ba41639c33647fb06108c8dfb2dc
s.martinez <santiskatersan@gmail.com>
2021-10-15 18:05:04 -0300

    clasify words into columns using POS

--- NLP/pipeline/nodes/POSLabeler.py
+++ NLP/pipeline/nodes/POSLabeler.py
@@ -9,8 +9,31 @@ class Labeler(PipelineNode):
 
     def handle(self, df: pd.DataFrame) -> pd.DataFrame:
         df['labeled_text'] = df['tokenized_text'].apply(self.label)
-        print(df)
+        df['noun'] = df['labeled_text'].apply(self.order_nouns)
+        df['adjective'] = df['labeled_text'].apply(self.order_adjectives)
+        df['verbs'] = df['labeled_text'].apply(self.order_verbs)
         return df
 
     def label(self, tokenized_tweet_text: list) -> list:
         return nltk.pos_tag(tokenized_tweet_text)
+
+    def order_nouns(self, labeled_text: list) -> list:
+        nouns = []
+        for index, value in labeled_text:
+            if value == 'NNP' or value == 'NN':
+                nouns.append(index)
+        return nouns
+
+    def order_verbs(self, labeled_text: list) -> list:
+        verbs = []
+        for index, value in labeled_text:
+            if value == 'VB':
+                verbs.append(index)
+            return verbs
+
+    def order_adjectives(self, labeled_text: list) -> list:
+        adjetives = []
+        for index, value in labeled_text:
+            if value == 'JJ' or value == 'RB':
+                adjetives.append(index)
+            return adjetives
