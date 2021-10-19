18aab75d43f69ff2417ca6b5d499eb4a78349bc5
s.martinez <santiskatersan@gmail.com>
2021-10-18 21:10:02 -0300

    lemmatizer but n| improved

--- NLP/pipeline/nodes/Lemmatizer.py
+++ NLP/pipeline/nodes/Lemmatizer.py
@@ -1,26 +1,25 @@
 import pandas as pd
 from .PipelineNode import PipelineNode
 from nltk.stem import WordNetLemmatizer
-from utils import create_wordcloud
+
 
 class Lemmatizer(PipelineNode):
     def __init__(self):
         self.__lemmatizer = WordNetLemmatizer()
 
     def handle(self, df: pd.DataFrame) -> pd.DataFrame:
-        ...
-        
-    def lemmatizer(self, df: pd.DataFrame):
+        df2 = df.copy()
+        return self.lemmatizer_fun(df2)
+
+    def lemmatizer_fun(self, df: pd.DataFrame):
         for row in df['tokenized_text']:
-            print("fila+1")
             for word in row:
                 if word in df['noun']:
-                    print(word)
-                    df['tokenized_text'][word] = self.__lemmatizer.lemmatize(word, "n")
+                    df['tokenized_text'] = df['tokenized_text'].replace([word], self.__lemmatizer.lemmatize(word, "n"))
                 elif word in df['adjective']:
-                    df['tokenized_text'][word] = self.__lemmatizer.lemmatize(word, "a")
+                    df['tokenized_text'] = df['tokenized_text'].replace([word], self.__lemmatizer.lemmatize(word, "a"))
                 elif word in df['verbs']:
-                    df['tokenized_text'][word] = self.__lemmatizer.lemmatize(word, "v")
+                    df['tokenized_text'] = df['tokenized_text'].replace([word], self.__lemmatizer.lemmatize(word, "v"))
         return df
 
 
