43921d293a07166da5eb25967f7602b6138de854
sebaf96 <sefernandez96@gmail.com>
2021-10-19 16:56:43 -0300

    Delete unnecesary method

--- NLP/pipeline/nodes/Lemmatizer.py
+++ NLP/pipeline/nodes/Lemmatizer.py
@@ -17,16 +17,3 @@ class Lemmatizer(PipelineNode):
         if word_type not in ('a', 'n', 'v', 'r'):
             raise Exception(f'LemmatizerException: word_type must be any of [a, n, v, r]. Got {word_type}')
         return [self.__lemmatizer.lemmatize(word, word_type) for word in words]
-
-    def lemmatizer_fun(self, df: pd.DataFrame):
-        for row in df['tokenized_text']:
-            for word in row:
-                if word in df['noun']:
-                    df['tokenized_text'] = df['tokenized_text'].replace([word], self.__lemmatizer.lemmatize(word, "n"))
-                elif word in df['adjective']:
-                    df['tokenized_text'] = df['tokenized_text'].replace([word], self.__lemmatizer.lemmatize(word, "a"))
-                elif word in df['verbs']:
-                    df['tokenized_text'] = df['tokenized_text'].replace([word], self.__lemmatizer.lemmatize(word, "v"))
-        return df
-
-
