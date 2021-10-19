a17e466f668b1f0697518592e516e096d8812f85
sebaf96 <sefernandez96@gmail.com>
2021-10-19 17:58:02 -0300

    Fix main

--- NLP/main.py
+++ NLP/main.py
@@ -10,7 +10,7 @@ from utils import download_packages_if_needed
 def search(word):
     click.secho("Searching tweets by specified parameters", fg="blue", bold=True)
     pipeline = NLPPipeline(word)
-    pipeline.add(Requester(max_pages=3))
+    pipeline.add(Requester(max_pages=5))
     pipeline.add(Cleaner(ignore_emojis=True))
     pipeline.add(Tokenizer())
     pipeline.add(Stopper())
@@ -18,19 +18,6 @@ def search(word):
     pipeline.add(Lemmatizer())
     pipeline.add(Polarizer())
     pipeline.run()
-
-    # # Tener en cuenta los download de nltk.download()
-    # # Debería poder guardar los múltiples hashtag
-    # response = twitter_request(hashtag, word, bearer_token)
-    # df = pd.json_normalize(response.json()['data'])
-    # print(df)
-    # df_clean = clean_data(df)
-    # print(df_clean)
-    # df_copy = df_clean
-    # tokenized_df = tokenize_df(df_copy)
-    # print(tokenized_df)
-    # print(count_words(tokenized_df))
-    # create_wordcloud(tokenized_df)
 
 
 if __name__ == '__main__':
