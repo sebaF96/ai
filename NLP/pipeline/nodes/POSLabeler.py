a40c6d8200fae92734726d677c09d5f84d096b05
sebaf96 <sefernandez96@gmail.com>
2021-10-19 23:58:56 -0300

    Add known_words and heuristics to Labeler

--- NLP/pipeline/nodes/POSLabeler.py
+++ NLP/pipeline/nodes/POSLabeler.py
@@ -5,7 +5,9 @@ from .PipelineNode import PipelineNode
 
 class Labeler(PipelineNode):
     def __init__(self):
-        ...
+        self.__known_nouns = ['messi', 'mbappe', 'neymar', 'psg', 'ucl', 'nba', 'nfl', 'barca', 'uefachampions league']
+        self.__known_verbs = []
+        self.__known_adjectives = ['goat']
 
     def handle(self, df: pd.DataFrame) -> pd.DataFrame:
         df['labeled_text'] = df['tokenized_text'].apply(self.label)
@@ -18,24 +20,22 @@ class Labeler(PipelineNode):
         return nltk.pos_tag(tokenized_tweet_text)
 
     def extract_nouns(self, labeled_text: list) -> list:
-        nouns = []
-        # Atributo de la clase -> para nouns, adjectives, verbs
-        exclude = ['messi', 'mbappe', 'neymar', 'psg', 'ucl', 'nba', 'nfl', 'barca', 'uefachampions league']
-        for word, tag in labeled_text:
-            if tag in ('NNP', 'NN', 'NNS', 'NNPS') or word in exclude:
-                nouns.append(word)
-        return nouns
+        def is_noun(word: str, tag: str) -> bool:
+            if word in self.__known_verbs or word in self.__known_adjectives:
+                return False
+            return tag in ('NNP', 'NN', 'NNS', 'NNPS') or word in self.__known_nouns
+        return [word for word, tag in labeled_text if is_noun(word, tag)]
 
     def extract_verbs(self, labeled_text: list) -> list:
-        verbs = []
-        for word, tag in labeled_text:
-            if tag in ('VB', 'VBG', 'VBD', 'VBN', 'VBP', 'VBZ'):
-                verbs.append(word)
-        return verbs
+        def is_verb(word: str, tag: str) -> bool:
+            if word in self.__known_nouns or word in self.__known_adjectives:
+                return False
+            return tag in ('VB', 'VBG', 'VBD', 'VBN', 'VBP', 'VBZ') or word in self.__known_verbs
+        return [word for word, tag in labeled_text if is_verb(word, tag)]
 
     def extract_adjectives(self, labeled_text: list) -> list:
-        adjectives = []
-        for word, tag in labeled_text:
-            if tag in ('JJ', 'JJR', 'JJS', 'RB'):
-                adjectives.append(word)
-        return adjectives
+        def is_adjective(word: str, tag: str) -> bool:
+            if word in self.__known_nouns or word in self.__known_verbs:
+                return False
+            return tag in ('JJ', 'JJR', 'JJS', 'RB') or word in self.__known_adjectives
+        return [word for word, tag in labeled_text if is_adjective(word, tag)]
