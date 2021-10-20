1f59cccd4ed08270fb892993e6588c99a5da0537
sebaf96 <sefernandez96@gmail.com>
2021-10-20 19:24:21 -0300

    Sanitize input to prevent numbers at the beginning

--- NLP/main.py
+++ NLP/main.py
@@ -7,6 +7,7 @@ from utils import download_packages_if_needed
 @click.command()
 @click.option('-word', prompt=True, help="Enter a word to search", default="")
 def search(word):
+    sanitize_input(word)
     click.secho("Searching tweets by specified parameters", fg="blue", bold=True)
     pipeline = NLPPipeline(word)
     pipeline.add(nd.Requester(max_pages=25))
@@ -18,6 +19,15 @@ def search(word):
     pipeline.add(nd.Polarizer())
     pipeline.add(nd.Printer())
     pipeline.run()
+
+
+def sanitize_input(word: str):
+    char_to_evaluate = word[0]
+    if char_to_evaluate == '#':
+        char_to_evaluate = word[1]
+    if char_to_evaluate.isdigit():
+        click.secho('La palabra o hashtag no puede comenzar con un número', fg='red', bold='true')
+        exit(1)
 
 
 if __name__ == '__main__':
